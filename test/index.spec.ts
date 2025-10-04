ef9020b01cbca1e4712d2888b586b8d48069985a
Clemens Zeidler <clemens.zeidler@gmail.com>
2025-10-05 12:26:15 +1300

    Add failing pluck() + first() tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,8 +2,7 @@ import Knex from "knex";
 import ClientPgLite from "../src/index";
 import { describe, it, expect, beforeAll } from "vitest";
 
-describe('Basic tests', () => {
-
+describe("Basic tests", () => {
   let db: Knex.Knex;
 
   beforeAll(() => {
@@ -11,27 +10,27 @@ describe('Basic tests', () => {
       client: ClientPgLite,
       connection: {
         // filename: './test-pg-data'
-      }
+      },
     });
   });
 
-  it('should run sql operations in the engine', async () => {
-    const result = await db.raw('select 1 + 1 as math');
+  it("should run sql operations in the engine", async () => {
+    const result = await db.raw("select 1 + 1 as math");
     expect(result).toBeTruthy();
     expect(2).toEqual(result?.rows[0]?.math);
   });
 
-  it('should be able to use schema builder', async () => {
-    await db.schema.createTable('todos', tb => {
+  it("should be able to use schema builder", async () => {
+    await db.schema.createTable("todos", (tb) => {
       tb.increments();
-      tb.string('description').notNullable();
-      tb.boolean('done').notNullable().defaultTo(false);
+      tb.string("description").notNullable();
+      tb.boolean("done").notNullable().defaultTo(false);
       tb.timestamps(true, true);
     });
 
-    const [result] = await db('todos')
-      .insert({ description: 'do the dishes' })
-      .returning('*');
+    const [result] = await db("todos")
+      .insert({ description: "do the dishes" })
+      .returning("*");
 
     expect(result).toBeTruthy();
     expect(result.id).toBeTruthy();
@@ -39,19 +38,36 @@ describe('Basic tests', () => {
     expect(result.updated_at).toBeTruthy();
   });
 
-  it('should be able to use PostgreSQL specific dialect operations', async () => {
-    await db.schema.createTable('contacts', tb => {
+  it("should be able to use PostgreSQL specific dialect operations", async () => {
+    await db.schema.createTable("contacts", (tb) => {
       tb.increments();
-      tb.string('name').notNullable();
+      tb.string("name").notNullable();
     });
 
-    await db('contacts').insert([
-      { name: 'Alice' }, { name: 'Bob' }, { name: 'Caesar' }, { name: 'David' }, { name: 'Edward' }
+    await db("contacts").insert([
+      { name: "Alice" },
+      { name: "Bob" },
+      { name: "Caesar" },
+      { name: "David" },
+      { name: "Edward" },
     ]);
 
-    const result = await db('contacts').whereILike('name', `%c%`)
+    const result = await db("contacts").whereILike("name", `%c%`);
     expect(result).toBeTruthy();
     expect(2).toEqual(result.length);
-    expect(result.map(r => r.name)).toContain('Caesar');
-  })
+    expect(result.map((r) => r.name)).toContain("Caesar");
+  });
+
+  it("should be able to pluck() columns", async () => {
+    const result = await db("contacts").pluck("id").whereILike("name", `%c%`);
+    expect(result).toBeTruthy();
+    expect(2).toEqual(result.length);
+    expect(result.every((r) => r.name === undefined)).toBe(true);
+  });
+
+  it("should be able to call first()", async () => {
+    const result = await db("contacts").first().orderBy("name");
+    expect(result).toBeTruthy();
+    expect(result.name).toBe("Alice");
+  });
 });
